162baa4da0f9b683e3a9991e5a6164dac29ed40c
anna-maiques <maiquesannacecilia@gmail.com>
2026-04-17 08:24:06 -0300

    Add files via upload

---
+++ aula03B.py
@@ -0,0 +1,60 @@
+# # Condicionais
+# #Operadoes Aritmeticos
+# num = 15
+# print(num)
+#
+# num = num + 2
+# print(num)
+#
+# num += 2
+# print(num)
+#
+# #Operadores Relacionais
+# print()
+# print(6 == 2)
+#
+# idade = 2
+# print(idade == 20)
+#
+# maior_idade = idade >= 18
+# print(maior_idade)
+#
+# #Operadores Lógicos
+# #LOGICA E (and)
+# print()
+#
+# verifica_email = True
+# verifica_senha = False
+#
+# login = verifica_senha and verifica_email
+# print(login)
+#
+# if login:
+#     print("kakakakaka")
+# else:
+#     print('Se fudeu')
+#
+# print()
+
+#Verificação de notas
+# nota_final = 8
+#
+# if nota_final < 7:
+#     print(" Reprovado ")
+#     print(" Repeteco ")
+# else:
+#     print(" Aprovado ")
+#     print(" Passou ")
+#
+# print(" Acabou ")
+
+nota_final = 7.3
+
+if nota_final <= 4:
+    print(" Repeteco ")
+elif nota_final < 6:
+    print(" Recuperação ")
+else:
+    print(" Aprovado ")
+
+print("Cabou")
